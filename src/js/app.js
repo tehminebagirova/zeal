76e6f3bd1130844814e72d5d74a5a33067e03270
Tahmina <tahmina.b@labrin.tech>
2020-08-08 13:58:57 +0400

    Feat: Menu created

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,18 @@
 'use strict';
 import 'bootstrap/dist/js/bootstrap';
 
-$(function() {
+$(function () {
 
-  $(document).on("click", "#alert", function(){
-    alert("clicked");
+  //#region MENU 
+  $('.menuButton').click(function () {
+    $(this).toggleClass('active');
+    $(this).toggleClass('no-hover');
+    $('#menu').slideToggle();
+    $('.inner-menu').toggleClass('active');
+    $('body').toggleClass('overflow-hidden');
+    // $('.navBar').slideToggle();
+    $('header').toggleClass('change');
+    $('.menu-logo li').toggleClass('change');
   });
+  //#endregion
 });
